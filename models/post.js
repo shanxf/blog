d8d96907850998398691d2327e8165e8fb0737f8
shanxf <ishanxf@gmail.com>
2014-06-27 00:12:18 +0800

    增加pv统计和留言统计

--- models/post.js
+++ models/post.js
@@ -28,7 +28,8 @@ Post.prototype.save = function(callback) {
   	title: this.title,
     tags: this.tags,
   	post: this.post,
-    comments: []
+    comments: [],
+    pv: 0
   }
   //打开数据库
   mongodb.open(function(err, db){
@@ -113,12 +114,25 @@ Post.getOne = function (name, day, title, callback) {
         "time.day": day,
         "title": title 
       }, function (err, doc) {
-        mongodb.close();
         if (err) {
+          mongodb.close();
           return callback(err);
         }
         //解析markdown为html
         if (doc) {
+          //每访问一次，pv值+1
+          collection.update({
+            "name": name,
+            "time.day": day,
+            "title": title
+          }, {
+            $inc: {"pv": 1}
+          }, function (err) {
+            mongodb.close();
+            if (err) {
+              return callback(err);
+            }
+          });
           doc.post = markdown.toHTML(doc.post);
           doc.comments.forEach(function (comment) {
             comment.content = markdown.toHTML(comment.content);
